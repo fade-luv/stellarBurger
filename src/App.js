6ffb9bfb1a4a728b291d8bbb1fdcf49f6bbd6a86
fade_luv <ssklvsk@gmail.com>
2023-02-01 23:14:31 +0300

    Сверстаны страницы входа и регистрации

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import  {LoginPage}  from "./pages/login";
 import  HomePage  from "./pages/HomePage";
+import  {RegisterPage}  from "./pages/register";
 
 function App(props) {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage /> } />
     </Routes>
   );
 }
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,35 +1,36 @@
 import React from "react";
-import pages from "./pages.module.css";
-import  AppHeader  from "../components/AppHeader/AppHeader";
+import AppHeader from "../components/AppHeader/AppHeader";
 import { EmailInput } from "@ya.praktikum/react-developer-burger-ui-components";
 import { PasswordInput } from "@ya.praktikum/react-developer-burger-ui-components";
 import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Input } from "@ya.praktikum/react-developer-burger-ui-components";
 import { Link } from "react-router-dom";
+import pages from "./pages.module.css";
 
-
-export function LoginPage(params) {
+export function RegisterPage(params) {
   return (
     <>
       <AppHeader />
       <form className={pages.container}>
         <h1 className={` ${pages.header} text text_type_main-medium mb-6`}>
-          Вход
+          Регистрация
         </h1>
+        <Input placeholder="Имя" extraClass="mb-6" />
         <EmailInput extraClass="mb-6" />
         <PasswordInput extraClass="mb-6" />
-        <div className={pages.sumbmit_login}>
+        <div className={pages.sumbmit_register}>
           <Button
             htmlType="button"
             type="primary"
             size="large"
             extraClass="mb-20"
           >
-            Войти
+            Зарегестрироваться
           </Button>
         </div>
 
         <p className={`${pages.newUser} mt-20`}>
-          Вы новый пользователь?
+          Уже зарегистрированы?
           <Link to="/register">
             <Button
               htmlType="button"
@@ -37,24 +38,13 @@ export function LoginPage(params) {
               size="small"
               extraClass="pl-2 pr-2"
             >
-              Зарегестрироваться
+              Войти
             </Button>
           </Link>
         </p>
-        <p className={pages.forgotPassword}>
-          Забыли пароль?
-          <Button
-            htmlType="button"
-            type="secondary"
-            size="small"
-            extraClass="pl-2 pr-2"
-          >
-            Восстановить пароль
-          </Button>
-        </p>
       </form>
     </>
-  );
-}
+  ); 
 
-  //  <p className={`${header.nav__link_title} ml-2`}>Конструктор</p>;
+
+}
